e73969cbea400eaef80c962ef3caf77a0cd0f150
Matias <mhumpirig@unjbg.edu.pe>
2025-06-18 22:26:11 -0500

    Simplificando el ejercicio

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-void convertirMayusculasIniciales(char texto[]) {
-    int nuevaPalabra = 1;
+int main() {
 
-    for (int i = 0; texto[i] != '\0'; i++) {
-        if (texto[i] == ' ') {
-            nuevaPalabra = 1;
+    char frase[] = "caceres patrimonio de la humanidad";
+
+    int nuevaPalabra = 1; 
+
+    for (int i = 0; frase[i] != '\0'; i++) {
+        if (frase[i] == ' ') {
+            nuevaPalabra = 1; 
         } else {
             if (nuevaPalabra == 1) {
-                if (texto[i] >= 'a' && texto[i] <= 'z') {
-                    texto[i] = texto[i] - 32;
+          
+                if (frase[i] >= 'a' && frase[i] <= 'z') {
+                    frase[i] = frase[i] - 32;
                 }
                 nuevaPalabra = 0;
             } else {
-                if (texto[i] >= 'A' && texto[i] <= 'Z') {
-                    texto[i] = texto[i] + 32;
+                if (frase[i] >= 'A' && frase[i] <= 'Z') {
+                    frase[i] = frase[i] + 32;
                 }
             }
         }
     }
-}
 
-int main() {
-    char texto[] = "caceres patrimonio de la humanidad";
-    convertirMayusculasIniciales(texto);
+    cout << frase << endl;
 
-    cout << "Resultado: " << texto << endl;
     return 0;
 }
 
